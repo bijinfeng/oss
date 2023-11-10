39697dda710d58a8c5f9350b1f08b27334818d15
Kebai <2804673379@qq.com>
2023-11-10 16:28:11 +0800

    feat: add some components

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -7,6 +7,38 @@ import { Form } from "~/components/form";
 import { CheckboxGroup } from "~/components/ui/checkbox";
 import type { FormInstance } from "~/components/form";
 import type { OptionItem } from "~/components/ui/checkbox";
+import { DatePickerWithRange } from "~/components/ui/date-picker";
+import { AlbumArtwork } from "~/components/album-artwork";
+import { Pagination } from "~/components/ui/pagination";
+// import { EmptyPlaceholder } from "~/components/empty-placeholder";
+import type { FileInfo } from "~/interface";
+
+export const madeForYouAlbums: Partial<FileInfo>[] = [
+  {
+    name: "Thinking Components",
+    url: "https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Functional Fury",
+    url: "https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80",
+  },
+  {
+    name: "React Rendezvous",
+    url: "https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Stateful Symphony",
+    url: "https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80",
+  },
+  {
+    name: "Async Awakenings",
+    url: "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+  },
+  {
+    name: "The Art of Reusability",
+    url: "https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80",
+  },
+];
 
 const options: OptionItem[] = [
   {
@@ -27,13 +59,16 @@ const PhotoFilterForm = () => {
       <Form.Item name="beds" label="图床">
         <CheckboxGroup options={options} />
       </Form.Item>
+      <Form.Item name="beds" label="上传时间">
+        <DatePickerWithRange />
+      </Form.Item>
     </Form>
   );
 };
 
 const Main = () => {
   return (
-    <div className="py-6">
+    <div className="py-6 h-full flex flex-col">
       <div className="flex items-center justify-between">
         <div className="space-y-1">
           <h2 className="text-2xl font-semibold tracking-tight">Gallery</h2>
@@ -47,8 +82,8 @@ const Main = () => {
           </Button>
         </div>
       </div>
-      <div className="flex mt-4 gap-4">
-        <div className="basis-1/4 space-y-4">
+      <div className="flex mt-4 gap-8 flex-1">
+        <div className="basis-1/5 space-y-4">
           <div className="py-2">
             <PhotoFilterForm />
           </div>
@@ -57,7 +92,15 @@ const Main = () => {
             Reset to defaults
           </Button>
         </div>
-        <div className="basis-3/4"></div>
+        <div className="basis-4/5 relative flex flex-col justify-between">
+          {/* <EmptyPlaceholder /> */}
+          <div className="flex space-x-4 pb-4">
+            {madeForYouAlbums.map((item, index) => (
+              <AlbumArtwork key={index} album={item as FileInfo} />
+            ))}
+          </div>
+          <Pagination />
+        </div>
       </div>
     </div>
   );

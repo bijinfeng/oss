20753772281385c509811a2686b0e4dc73b87817
Kebai <2804673379@qq.com>
2023-11-25 18:49:00 +0800

    feat: add avatar upload component

--- src/components/avatar-upload.tsx
+++ src/components/avatar-upload.tsx
@@ -1,5 +1,6 @@
 import { useControllableValue } from "ahooks";
 import Dropzone from "react-dropzone";
+import { ArrowUpFromLine } from 'lucide-react';
 
 import { Avatar, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
@@ -9,22 +10,27 @@ interface AvatarUploadProps {
   onChange?: (value: string) => void;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const AvatarUpload = (props: AvatarUploadProps) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [avatarUrl, setAvatarUrl] = useControllableValue<string>();
+  const [avatarUrl, setAvatarUrl] = useControllableValue<string>(props);
 
   return (
     <Dropzone>
       {({ getRootProps, getInputProps }) => (
         <div className="flex">
           <input {...getInputProps()} />
-          <Avatar {...getRootProps()}>
+          <Avatar {...getRootProps()} className="w-20 h-20 cursor-pointer relative group">
             <AvatarImage src={avatarUrl} />
+            <div className="absolute w-full h-full top-0 left-0 flex items-center justify-center bg-black/60 border box-border rounded-full invisible group-hover:visible">
+              <ArrowUpFromLine width="16" height="16" />
+            </div>
           </Avatar>
-          <div>
-            <Button {...getRootProps()}>更新头像</Button>
-            <p>可以拖动图片到左边头像区域完成上传</p>
+          <div className="ml-4 my-auto">
+            <Button size="sm" className="h-6 px-2 inline-flex items-center gap-1" {...getRootProps()}>
+              <ArrowUpFromLine width="14" height="14" />
+              更新头像
+            </Button>
+            <p className="text-xs text-muted-foreground mt-2">可以拖动图片到左边头像区域完成上传</p>
           </div>
         </div>
       )}
--- src/components/profile-form.tsx
+++ src/components/profile-form.tsx
@@ -2,13 +2,17 @@ import { useRef } from "react";
 import Form from "@/components/form";
 import type { FormInstance } from "@/components/form";
 import { AvatarUpload } from "@/components/avatar-upload";
-// import { getDefaultAvatar } from "@/lib/utils";
+import { getDefaultAvatar } from "@/lib/utils";
+
+interface FormValue {
+  avatar: string;
+}
 
 export const ProfileForm = () => {
-  const formRef = useRef<FormInstance>(null);
+  const formRef = useRef<FormInstance<FormValue>>(null);
 
   return (
-    <Form form={formRef}>
+    <Form<FormValue> form={formRef} defaultValues={{ avatar: getDefaultAvatar("kebai") }}>
       <Form.Item name="avatar" label="头像">
         <AvatarUpload />
       </Form.Item>
